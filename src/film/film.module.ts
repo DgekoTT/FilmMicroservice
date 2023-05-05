c38eb87866aa78c8e5297d4eeac0f6e106fbf6c7
DgekoTT <106529200+DgekoTT@users.noreply.github.com>
2023-05-06 00:02:45 +0300

    Update film.module.ts

--- src/film/film.module.ts
+++ src/film/film.module.ts
@@ -22,7 +22,7 @@ import {ClientsModule, Transport} from "@nestjs/microservices";
           name: 'FILM_SERVICE',
           transport: Transport.RMQ,
           options: {
-            urls: ['amqp://localhost:5672'],
+            urls: ['amqp://rabbitmq:5672'],
             queue: 'films_queue',
             queueOptions: {
               durable: false
