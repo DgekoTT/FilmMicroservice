58833cf17e9dc69b1cb73ed74d334ffc321f7213
Dgeko <dfhdfh13@gmail.com>
2023-05-09 11:25:56 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {GenreService} from "./genre/genre.service";
-
+import {CorsOptions} from "@nestjs/common/interfaces/external/cors-options.interface";
+import * as cors from 'cors';
 
 
 
 async function server() {
   const PORT = process.env.PORT || 4050;
   const app = await NestFactory.create(AppModule);
+  // Использовать * для разрешения любого источника
+  const corsOptions: CorsOptions = {
+    origin: '*',
+  };
+  app.use(cors(corsOptions));
   await app.listen(PORT, () => console.log(`Server Films is started on PORT = ${PORT} `))
 }
 
