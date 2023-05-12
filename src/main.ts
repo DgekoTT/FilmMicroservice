c8b71c2d46e7aefe89dd651443b0b284c5ef4ab5
Dgeko <dfhdfh13@gmail.com>
2023-05-12 19:40:13 +0300

    fix cors options

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,14 @@ import { AppModule } from './app.module';
 
 async function server() {
   const PORT = process.env.PORT || 4050;
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "*",
+    credentials: true,// отвечает за куки
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 200
+  });
   await app.listen(PORT, () => console.log(`Server Films is started on PORT = ${PORT} `))
 }
 
