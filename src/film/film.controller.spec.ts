a9e2f87b47c3d91e098d577d49ae60d54a7f4ef3
qwestern13 <qwestern13@yandex.ru>
2023-05-31 14:09:23 +0500

    added test filmController

---
+++ src/film/film.controller.spec.ts
@@ -0,0 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { FilmController } from './film.controller';
+import { FilmService } from './film.service';
+import { JwtService } from '@nestjs/jwt';
+
+describe('FilmControllerTest', () => {
+  let filmController: FilmController;
+  let spyService: FilmService;
+
+  const mockFilmService = {
+    
+  }
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [FilmController],
+      providers: [
+        FilmService,
+        JwtService
+      ]
+    }).overrideProvider(FilmService)
+    .useValue(mockFilmService)
+    .compile();
+
+    filmController = module.get<FilmController>(FilmController);
+    spyService = module.get<FilmService>(FilmService);
+  });
+
+  it('should be defined', () => {
+    expect(filmController).toBeDefined();
+  });
+
+  it('should be call loadActors', () => {
+  });
+});
