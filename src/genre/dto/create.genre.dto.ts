497f47bbddd8a847782a147cb34e18a422af5647
DgekoTT <106529200+DgekoTT@users.noreply.github.com>
2023-06-14 00:17:51 +0300

    add swager

--- src/genre/dto/create.genre.dto.ts
+++ src/genre/dto/create.genre.dto.ts
@@ -1,5 +1,12 @@
- export class CreateGenreDto {
-    readonly name: string;
+import {ApiProperty} from "@nestjs/swagger";
+import {IsString} from "class-validator";
+
+export class CreateGenreDto {
+    @ApiProperty({example: 'драма', description: 'название жанра на русском'})
+    @IsString({message: " Должно быть строкой"})
     readonly nameRu: string;
+
+    @ApiProperty({example: 'drama', description: 'название жанра на английском'})
+    @IsString({message: " Должно быть строкой"})
     readonly nameEn: string;
  }
--- src/genre/dto/update.genre.dto.ts
+++ src/genre/dto/update.genre.dto.ts
@@ -1,5 +1,16 @@
- export class UpdateGenreDto {
+import {ApiProperty} from "@nestjs/swagger";
+import {IsNumber, IsString} from "class-validator";
+
+export class UpdateGenreDto {
+    @ApiProperty({example: '1', description: 'Уникальный идентификатор'})
+    @IsNumber({}, {message: " Должно быть числом"})
     readonly id: number;
-     readonly nameRu: string;
-     readonly nameEn: string;
+
+    @ApiProperty({example: 'драма', description: 'название жанра на русском'})
+    @IsString({message: " Должно быть строкой"})
+    readonly nameRu: string;
+
+    @ApiProperty({example: 'drama', description: 'название жанра на английском'})
+    @IsString({message: " Должно быть строкой"})
+    readonly nameEn: string;
  }
